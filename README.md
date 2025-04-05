4eeeb1208a93d3546e62d4da061476ac2e80c0d3
Salman-Ahamed <me.shahriyarhosen@gmail.com>
2025-04-05 16:44:27 +0600

    initialize Vite project with React and Tailwind CSS; add basic dashboard components and assets

---
+++ README.md
@@ -0,0 +1,40 @@
+1. Basic react dashboard setup done with vite;
+
+2. tailwindcss install done with "print hello world";
+3. don't forget to install react-icons to install this type on the terminal bellow code
+
+```js
+npm i react-icons --save-dev
+```
+
+## Part 1
+
+React Router Setup done and some css has been added for basic routing styling and all page components have created
+
+## Part 2
+
+navbar of the dashboard has been created and has been copyed assets folder and impliment chart js also react-chartjs-2. and created button component also contentHeader components.
+
+###### install chart js type on the terminal
+
+```js
+npm install chart.js
+```
+
+###### install react-chartjs-2 type on the terminal
+
+```js
+npm i react-chartjs-2
+```
+
+## Part 3
+
+profile page created done
+
+## part 4
+
+Order page created done
+
+## Part 5
+
+Proudcts page created done
---
+++ src/components/Dashboard/TodaySales.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import WrapperDiv from "./WrapperDiv";
+import ContentHeader from "../ContentHeader";
+import { SalesData } from "../../data/index";
+import Bars from "../../assets/Bars.png";
+import ChartGraph from "../chart/chart";
+
+const SalesDetails = () => {
+  return (
+    <div className="p-4">
+      <ContentHeader title="Today’s Sales" des="Sales Summary" />
+      <div className=" grid grid-cols-2 lg:grid-cols-4 gap-5">
+        {SalesData &&
+          SalesData.map((data) => {
+            return (
+              <div key={data.id} className={`bg-primary1 p-4 rounded-md`}>
+                <img src={data.icon} alt="" />
+                <div className="mt-2">
+                  <p className="text-[15px] font-semibold text-white/80 pb-1">
+                    {data.count}
+                  </p>
+                  <h1 className="text-white/80 text-[10px] md:text-[12px] font-medium">
+                    {data.title}
+                  </h1>
+                  <p
+                    style={{ color: data.color }}
+                    className={`text-[8px] md:text-[10px] font-medium`}
+                  >
+                    {data.des}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+const SalesChart = () => {
+  return (
+    <div className="flex-0.2 p-4">
+      <ContentHeader title="Level" />
+      <div className="w-full h-[120px]">
+        {/* <img className="w-full h-full" src={Bars} alt="" /> */}
+        <ChartGraph />
+      </div>
+      <div className="flex justify-center w-full gap-20 lg:gap-0 items-center pt-2 mt-2">
+        <button className="flex items-center lg:justify-center gap-2 text-grayText text-[10px] font-medium lg:border-r border-grayText/30 lg:w-full">
+          <span className="h-2 w-2 rounded-full bg-secondary"></span>Volume
+        </button>
+        <button className="flex items-center lg:justify-center gap-2 text-grayText text-[10px] font-medium lg:w-full">
+          <span className="h-2 w-2 rounded-full bg-grayText"></span>Service
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const TodaySales = () => {
+  return (
+    <div>
+      <WrapperDiv>
+        <div className="flex-[1.3] lg:flex-[1.5] bg-primary2 rounded-md">
+          <SalesDetails />
+        </div>
+        <div className="flex-[0.7] lg:flex-[0.5] bg-primary2 rounded-md">
+          <SalesChart />
+        </div>
+      </WrapperDiv>
+    </div>
+  );
+};
+
+export default TodaySales;
